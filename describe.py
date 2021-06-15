3eee45b0f9f0101db089386d2d7d42fe7207825b
raf <rbalbous@student.42.fr>
2021-06-15 11:46:47 +0200

    final version

--- describe.py
+++ describe.py
@@ -4,46 +4,48 @@ import sys
 import time
 from tools import *
 
+def print_elem(element, len_data):
+		print("max:    ", end = '')
+		for i in range (6, len_data):
+			print("%15.2f" % {element}, end = '')
+		print('')
 
 def init_tab(data):
 	name = [x for x in data[0]]
 	len_data = len(data[0])
 	elem = [[row[i] for row in data] for i in range(len_data)]
-	# len_data = 10
-	print("          ", end = '')
+	print("        ", end = '')
 	for i in range (6, len_data):
-		print("%-15.9s" % name[i], end = '')
+		print("%15.10s" % name[i], end = '')
 	print('')
 	print("max:    ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % max(elem[i][1:]), end = '')
+		print("%15.2f" % max(elem[i][1:]), end = '')
 	print('')
 	print("min:    ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % min(elem[i][1:]), end = '')
+		print("%15.2f" % min(elem[i][1:]), end = '')
 	print('')
 	print("mean:   ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % mean(elem[i][1:]), end = '')
+		print("%15.2f" % mean(elem[i][1:]), end = '')
 	print('')
 	print("std:    ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % std(elem[i][1:]), end = '')
+		print("%15.2f" % std(elem[i][1:]), end = '')
 	print('')
 	print("q1:     ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % q1(elem[i][1:]), end = '')
+		print("%15.2f" % q1(elem[i][1:]), end = '')
 	print('')
 	print("q3:     ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % q3(elem[i][1:]), end = '')
+		print("%15.2f" % q3(elem[i][1:]), end = '')
 	print('')
 	print("median: ", end = '')
 	for i in range (6, len_data):
-		print("%-15.2f" % median(elem[i][1:]), end = '')
+		print("%15.2f" % median(elem[i][1:]), end = '')
 	print('')
-	# for i in range(6, len_data):
-		# print(f"max: {max(elem[i][1:])}, min(elem[i][1:]), mean(elem[i][1:]), std(elem[i][1:]), q1(elem[i][1:]), q3(elem[i][1:]), median(elem[i][1:])")
 
 
 def main():
--- histogram.py
+++ histogram.py
@@ -1,13 +1,14 @@
 import csv
 import sys
 import matplotlib.pyplot as plt
+from tools import mean
 
 def init_tab(data):
     name = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
-    house = [[item for item in data if item[1] == name[i]] for i in range(4)]
+    house = [[item[6:] for item in data if item[1] == name[i]] for i in range(4)]
+    subjects = [subj for subj in data[0]][6:]
 
-
-    fig = plt.figure(figsize=(25, 15), facecolor='beige')
+    fig = plt.figure(figsize=(25, 15), facecolor='grey')
     fig.canvas.set_window_title('[Histogram]')
     color = {'Gryffindor':'crimson', 'Slytherin':'green', 'Ravenclaw':'teal', 'Hufflepuff':'purple'}
     plt.subplot(5, 3, 1)
@@ -19,11 +20,13 @@ def init_tab(data):
     plt.yticks([], [])
     plt.title('Houses')
     for i, feature in enumerate(house):
-        plt.subplot(5, 3, i + 2)
-        plt.title(feature)
+        print(i)
+        plt.subplot(5, 3, i + 1)
+        plt.title(subjects[i])
         plt.grid(True)
         for idx, item in enumerate(name):
-            plt.hist(item, 20, facecolor=color[name[idx]], alpha=0.5)
+            print(feature[:idx])
+            plt.hist(mean(feature[idx]), 20, color=color[idx], alpha=0.5)
     plt.subplots_adjust(top=0.9, bottom=0.1, left=0.1, right=0.9, hspace=0.5, wspace=0.3)
     plt.show()  
 
